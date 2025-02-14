95e980dd89cd8ab142b4a1479448b55fcfe762b1
Mu-abdullah <moabdullah588@gmail.com>
2025-02-14 17:20:07 +0200

    update AI generat

--- lib/core/services/ai/ai_setup.dart
+++ lib/core/services/ai/ai_setup.dart
@@ -7,4 +7,15 @@ class AiSetup {
   static final String apiKey = '${dotenv.env['GOOGLE_GEMINI_API']}';
 
   static GenerativeModel get generativeAI => locator<GenerativeModel>();
+
+  static Future<String> aiGenerate({required String promot}) async {
+    var generativeAI = AiSetup.generativeAI;
+
+    var prompt = promot;
+
+    final content = [Content.text(prompt)];
+    final response = await generativeAI.generateContent(content);
+
+    return response.text!;
+  }
 }
--- lib/features/city_screen/data/model/city_bio_creator.dart
+++ lib/features/city_screen/data/model/city_bio_creator.dart
@@ -1,17 +1,11 @@
-import 'package:google_generative_ai/google_generative_ai.dart';
-
 import '../../../../core/services/ai/ai_setup.dart';
 
 class CityBioCreator {
   Future<String> generateCityBio(String city) async {
-    var generativeAI = AiSetup.generativeAI;
+    var response = await AiSetup.aiGenerate(
+      promot: "short description about $city with 100 words in arabic language",
+    );
 
-    var prompt =
-        "short description about $city with 100 words in arabic language";
-
-    final content = [Content.text(prompt)];
-    final response = await generativeAI.generateContent(content);
-
-    return response.text!;
+    return response;
   }
 }
