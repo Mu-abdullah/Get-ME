8d248a03d3d7438ae699dd5ec6ec0075c58c4325
Mu-abdullah <moabdullah588@gmail.com>
2025-03-25 12:28:14 +0200

    change ai promot and refactor governorate bio widgets

--- lib/features/governorate_screen/data/repo/city_bio_creator.dart
+++ lib/features/governorate_screen/data/repo/city_bio_creator.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
+
 import '../../../../core/services/ai/ai_setup.dart';
 
 class CityBioCreator {
   Future<String> generateCityBio(String city) async {
     var response = await AiSetup.aiGenerate(
       promot:
-          "A description without introduction about $city Governorate, the best time to visit, and its famous cities or places in 150 words in Arabic",
+          "A description without introduction about $city Governorate, the best time to visit, and its most famous cities or places, in bullet points of 150 words in Arabic, to be displayed in a phone application.",
     );
+    debugPrint("CityBioCreator: $response");
     return response;
   }
 }
--- lib/features/governorate_screen/presentation/refactor/city_bio.dart
+++ lib/features/governorate_screen/presentation/refactor/city_bio.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:getme/core/extextions/extentions.dart';
 
+import '../../../../core/style/color/color_light.dart';
+import '../../../../core/style/font/fonts_helper.dart';
 import '../../../../core/style/widgets/app_text.dart';
 import '../../../../core/style/widgets/custom_shimmer.dart';
 import '../cubits/city_bio_creator_cubit/city_bio_creator_cubit.dart';
@@ -30,17 +32,80 @@ class CityBio extends StatelessWidget {
   SliverToBoxAdapter _buildCityCreator(
       CityBioGenerated state, BuildContext context) {
     return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: AppText(
-          state.bio.trim(),
-          maxLines: 20,
-          textAlign: TextAlign.justify,
-          height: 2,
-          fontSize: context.bodySmall!.fontSize,
+      child: InkWell(
+        onTap: () async =>
+            await Clipboard.setData(ClipboardData(text: state.bio)),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                height: 2,
+              ),
+              children: _parseBoldText(state.bio),
+            ),
+          ),
+
+          // AppText(
+          //   state.bio.trim(),
+          //   maxLines: 40,
+          //   textAlign: TextAlign.justify,
+          //   height: 2,
+          //   fontSize: context.bodySmall!.fontSize,
+          // ),
         ),
       ),
     );
+  }
+
+  List<TextSpan> _parseBoldText(String text) {
+    final RegExp boldPattern = RegExp(r'\*\*(.*?)\*\*'); // البحث عن النص بين **
+    List<TextSpan> spans = [];
+    int lastMatchEnd = 0;
+
+    for (final match in boldPattern.allMatches(text)) {
+      // إضافة النص العادي قبل المطابقة
+      if (match.start > lastMatchEnd) {
+        spans.add(TextSpan(
+          text: text.substring(lastMatchEnd, match.start),
+          style: TextStyle(
+            fontFamily: FontsHelper.fontFamily,
+            fontSize: 12,
+            fontWeight: FontWeight.normal,
+            color: ColorsLight.black.withValues(alpha: 0.6),
+          ),
+        ));
+      }
+      // إضافة النص الغامق بدون **
+      spans.add(
+        TextSpan(
+          text: match.group(1), // النص بدون **
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: FontsHelper.fontFamily,
+          ),
+        ),
+      );
+      lastMatchEnd = match.end;
+    }
+
+    // إضافة أي نص متبقٍ بعد آخر تطابق
+    if (lastMatchEnd < text.length) {
+      spans.add(TextSpan(
+        text: text.substring(lastMatchEnd),
+        style: TextStyle(
+          fontFamily: FontsHelper.fontFamily,
+          fontSize: 12,
+          fontWeight: FontWeight.normal,
+          fontStyle: FontStyle.normal,
+          color: ColorsLight.black.withValues(alpha: 0.6),
+        ),
+      ));
+    }
+
+    return spans;
   }
 
   Widget _buildCityCreatorLoading() {
