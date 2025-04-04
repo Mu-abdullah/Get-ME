46f60e0bdfa5e045cbed5fb7f527d5420cf85c60
Mu-abdullah <moabdullah588@gmail.com>
2025-04-04 13:38:09 +0200

    refactor place background in home screen

--- lib/features/home_screen/presentation/widgets/home/places/places_body.dart
+++ lib/features/home_screen/presentation/widgets/home/places/places_body.dart
@@ -4,6 +4,7 @@ import 'package:getme/core/extextions/extentions.dart';
 
 import '../../../../../../core/language/lang_keys.dart';
 import '../../../../../../core/routes/routes_name.dart';
+import '../../../../../../core/style/statics/app_statics.dart';
 import '../../../../../../core/style/widgets/app_space.dart';
 import '../../../../../../core/style/widgets/no_place_found.dart';
 import '../../../cubits/home_places_cubit/home_places_cubit.dart';
@@ -24,28 +25,34 @@ class PlacesBody extends StatelessWidget {
           if (state.placesWithImages.isEmpty) {
             return NoPlaceFound();
           }
-          return Column(
-            children: [
-              const AppSpace(space: 5),
-              HomeSectionHeader(
-                title: LangKeys.places,
-                onTap: () {
-                  context.pushNamed(RoutesNames.allPlaces);
-                },
+          return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: AppBorderRadius.mediumRadius,
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.placesWithImages.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    final place = state.placesWithImages.keys.elementAt(index);
-                    final images = state.placesWithImages[place]!;
-                    return PlaceCard(place: place, images: images);
-                  },
-                ),
-              ),
-            ],
-          );
+              child: Column(
+                children: [
+                  const AppSpace(space: 5),
+                  HomeSectionHeader(
+                    title: LangKeys.places,
+                    onTap: () {
+                      context.pushNamed(RoutesNames.allPlaces);
+                    },
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: state.placesWithImages.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final place =
+                            state.placesWithImages.keys.elementAt(index);
+                        final images = state.placesWithImages[place]!;
+                        return PlaceCard(place: place, images: images);
+                      },
+                    ),
+                  ),
+                ],
+              ));
         } else if (state is PlacesError) {
           return Center(child: Text(state.message));
         }
